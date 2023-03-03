2005a40ae038a51a4d15a0aea25880a93fdb8c54
cefelix <felix.zeh@gmx.de>
2023-03-03 15:03:36 +0100

    added very basic scatterplots for EI, CI, SI and MI

--- exploratory plotting/exploratory_scatterplots.R
+++ exploratory plotting/exploratory_scatterplots.R
@@ -4,4 +4,29 @@
 #and https://github.com/cefelix/dBEF-Nematode-community/blob/main/wrangling/community%20indices.R
 #before, as this will put the data into the necessary format
 
+####libraries####
+library(tidyverse)
+
+####Enrichment index####
+#scatterplot
+ggplot(data.indices, aes(x = sowndiv, y = EI, color = treatment))+
+  geom_point()+
+  scale_x_continuous(trans = 'log2')
+
+####Channel ratio####
+#scatterplot
+ggplot(data.indices, aes(x = sowndiv, y = CI, color = treatment))+
+  geom_point()+
+  scale_x_continuous(trans = 'log2')
+  #here something with the maRcel::nemaplex function must have gone wrong
+
+####Structure index####
+ggplot(data.indices, aes(x = sowndiv, y = SI, color = treatment))+
+  geom_point()+
+  scale_x_continuous(trans = 'log2')
+
+####Maturity index####
+ggplot(data.indices, aes(x = sowndiv, y = MI, color = treatment))+
+  geom_point()+
+  scale_x_continuous(trans = 'log2')
 
