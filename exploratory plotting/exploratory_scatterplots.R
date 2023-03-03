ee740490e83f9318c7576a857e0dcc8cc12a5aa3
cefelix <felix.zeh@gmx.de>
2023-03-03 14:46:21 +0100

    defining aim of this code

--- exploratory plotting/exploratory_scatterplots.R
+++ exploratory plotting/exploratory_scatterplots.R
@@ -1,1 +1,7 @@
-#hello scatterplots, this is a test
+#creating some exploratory scatterplots.
+
+#always run https://github.com/cefelix/dBEF-Nematode-community/blob/main/wrangling/amynang-JenaXP_SP6_2021-wrangling-nematodes.R
+#and https://github.com/cefelix/dBEF-Nematode-community/blob/main/wrangling/community%20indices.R
+#before, as this will put the data into the necessary format
+
+
