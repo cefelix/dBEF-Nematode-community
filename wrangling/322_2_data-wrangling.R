afe0476a87d622c2e64c8f8430b949a3903efea1
cefelix <felix.zeh@gmx.de>
2023-09-11 14:19:27 +0200

    fixed unloaded package error

--- wrangling/322_2_data-wrangling.R
+++ wrangling/322_2_data-wrangling.R
@@ -1,15 +1,19 @@
 #structuring the vogel data
 
 library(tidyr)
+library(dplyr)
 
 raw <- read.csv("./322_2_data.csv") #as in JEXIS
   str(raw)
 
-data <- raw %>% 
+  
+data_vog <- raw %>% 
   group_by(genus) %>%
   mutate(row = row_number()) %>%
   pivot_wider(names_from = genus, values_from = number) %>%
   select(-row)
+
+
 
 #now we need soil FW and DW in order to work with the densities
 
