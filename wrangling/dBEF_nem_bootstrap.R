54072d358962d420d7c83842abb4c9f7d2db76ae
cefelix <felix.zeh@gmx.de>
2023-10-13 14:16:42 +0200

    storing bootstrapped df's in a list instead of an array
    inserting independent variables in each of the bootstrapped df's

--- wrangling/dBEF_nem_bootstrap.R
+++ wrangling/dBEF_nem_bootstrap.R
@@ -16,17 +16,6 @@ library(tidyverse)
 library(maRcel)
 library(vegan)
 
-#data:
-dBEF_nem <- read.csv("./dBEF_nem.csv", row.names = 1)
-
-
-####standardize taxon abundances:####
-taxa <- grep("Acrobeles", colnames(dBEF_nem)):ncol(dBEF_nem)
-dBEF_nem[taxa] <- decostand(dBEF_nem[taxa], method = "total", MARGIN = 1)
-  #check it:
-  rowSums(dBEF_nem[taxa])
-  
-
 
 ####bootstrap function doing almost the same as maRcel::extrapolate.robust####
 #creating data:
@@ -45,42 +34,71 @@ n = 3
   #n = number of iterations
 
 bootstrap_abuns <- function(taxa, counts, n) {
-  #an array to store the bootstrapped abundances for each of the n iterations:
-  bs_array <- array(dim = c( nrow(taxa), ncol(taxa), n),
-                    dimnames = list( c(rownames(taxa)),
-                                     c(colnames(taxa)),
-                                     seq(1:n)) )#taxa has to be a df
+  #a list to store the df's of bootstrapped abundances for each of the n iterations:
+  bs_list <- list(as.data.frame(matrix(nrow = nrow(taxa),
+                                       ncol = ncol(taxa),
+                                       dimnames = list( c(rownames(taxa)),
+                                                        c(colnames(taxa)) )
+                                       )) )
   #this loop is for the n iterations
   for (i in 1:n){
+    #initialize a data frame:
+    df <- as.data.frame(matrix(nrow = nrow(taxa),
+                               ncol = ncol(taxa)))
     #this loop re-samples a community for each site:
     for (j in 1:nrow(taxa)){
       
       draws <- sample(colnames(taxa), size = counts[j], replace = TRUE, prob = taxa[j,]) %>%
         as.factor() %>%
-        summary() # a vector with the number of draws for each species
+        summary() # a named vector with the number of draws for each species
       site_freq <- draws[colnames(taxa)] # bootsprapped composition at site j
       site_freq <- site_freq %>% replace(is.na(.), 0) #the NA's mean that we didn't sample any individual 
       
-      bs_array[j, , i] <- site_freq
+      df[j,] <- site_freq
       
     }
+    rownames(df) <- rownames(taxa)
+    colnames(df) <- colnames(taxa)
+    bs_list[[i]] <- df
     print(i) #to see progress
   }
-  #return the filled array
-  return(bs_array)
+  #return the list of data frames
+  return(bs_list)
 }
 
 
-#testing it on dBEF_nem
-taxa <- dBEF_nem[c(grep("Acrobeles", colnames(dBEF_nem)):ncol(dBEF_nem))]
-counts <- dBEF_nem$total_nematodes
+#### standardizing dBEF_nem ####
+  dBEF_nem <- read.csv("./dBEF_nem.csv", row.names = 1)
+  #standardize taxon abundances:#
+  stand <- grep("Acrobeles", colnames(dBEF_nem)):ncol(dBEF_nem)
+    dBEF_nem[stand] <- decostand(dBEF_nem[stand], method = "total", MARGIN = 1)
+    #check it:
+    rowSums(dBEF_nem[stand])
+  
+#### bootstrapping dBEF_nem ####  
+  #subset dBEF_nem to feed into bootstrap_abuns():
+    taxa <- dBEF_nem[c(grep("Acrobeles", colnames(dBEF_nem)):ncol(dBEF_nem))]
+    counts <- dBEF_nem$total_nematodes
+  #re-sample:    
+  resampled <- bootstrap_abuns(taxa, counts, n=100)
+  
+  #Insert independent variables to each bootstrapped df:
+  independent <- dBEF_nem[,1:15] #select independent vars
+  resampled <- lapply(resampled, cbind, independent) #add them
+  resampled <- lapply(resampled, relocate, c(81:95), .before = 1) #move to front
+  
+  #save the result:
+  save(resampled, file = "./wrangling/taxons_bootstrapped_list.RData")
 
-dBEF_bs <- bootstrap_abuns(taxa, counts, n=100)
 
-dBEF_bs[1:3,2:6,1:5]
 
-#save the result:
-save(dBEF_bs, file = "./dBEF_bootstrapped.RData")
+####repeat calculation of functional indices for each iteration####
+
+dBEF_bs <- array(rep(dBEF_nem, 100))
+
+
+
+
 
 #this is a test comment to resolve error: git did not send all necessary objects
 
