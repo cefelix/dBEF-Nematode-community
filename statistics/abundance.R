3bf4750fdc73994da3174e0fedc38565b04b8929
cefelix <felix.zeh@gmx.de>
2023-11-06 14:04:02 +0100

    fit a poisson and a neg-binomial model for abundance as counts with soilDW as an offset: total_nematodes ~ sowndivLogStd*treatment + offset(log(soilDW)) + (1|block/plot)

--- statistics/abundance.R
+++ statistics/abundance.R
@@ -331,7 +331,7 @@ m.counts.13 <- update(m.counts.11,
 pp_check(m.counts.11)
 pp_check(m.counts.13, ndraws=100) #bimodal, not fitting at all
 #same for 17 data
-m.counts.11a <- brm(total_nematodes ~ sowndiv*treatment + (1|block), 
+m.counts.11a <- brm(total_nematodes ~ sowndiv*treatment + offset(log(soilDW)) + (1|block), 
                    data = dBEF_nem17, family = "poisson",
                    chains = 3,
                    cores = 3,
@@ -358,3 +358,98 @@ save(m.counts.11,
 
 dBEF_nem21$total_nematodes %>% density() %>% plot()
 dBEF_nem21$ind_per100g %>% density() %>% plot()
+
+
+#### modelling abundance as a count with soilDW as an offset variable ####
+
+#https://en.wikipedia.org/wiki/Poisson_regression#:~:text=In%20these%20examples%2C%20exposure%20is,right%20side%20of%20the%20equation.
+#https://www.dataquest.io/blog/tutorial-poisson-regression-in-r/
+#https://github.com/paul-buerkner/brms/issues/98
+  #offset() is usable as in other packages
+
+#### total_nematodes ~ sowndivLogStd*treatment + offset(log(soilDW)) + (1|block/plot), fam=poisson ####
+#as the link function for poisson is log(), lets log transform offset variable:
+dBEF_nem21$soilDW %>% log() %>% density() %>% plot() #bimodal
+  #lets check plotwise:
+  ggplot(data = dBEF_nem21, aes(x = sowndivLog, y = soilDW))+
+    geom_point()+
+    facet_wrap(~block)
+  
+#the response variables density
+dBEF_nem21$total_nematodes  %>% density() %>% plot()
+rpois(1e3, lambda = 2) %>% density() %>% plot()
+SEED = 22061996
+
+m.abun.PoisOffS.21 <- brm(total_nematodes ~ sowndivLogStd*treatment + offset(log(soilDW)) + (1|block/plot), 
+                          data = dBEF_nem21, family = "poisson",
+                          chains = 3,
+                          cores = 3,
+                          iter = 3000, warmup = 1500,
+                          seed = SEED,
+                          control = list(adapt_delta = 0.9) ) 
+                          #74 divergent transitions,
+                          #291 exceeded max_treedepth
+
+m.abun.PoisOffS.22 <- update(m.abun.PoisOffS.21,
+                             seed = SEED,
+                             control = list(adapt_delta = 0.999,
+                                            max_treedepth = 12)) 
+                              #427 transitions exceeded max_treedepth
+pp_check(m.abun.PoisOffS.22, ndraws=100) #thats at least a better fit than m.[...].14
+
+
+m.abun.PoisOffS.23 <- update(m.abun.PoisOffS.22,
+                             seed = SEED,
+                             control = list(adapt_delta = 0.999,
+                                            max_treedepth = 15))
+                              #EES too low
+
+m.abun.PoisOffS.24 <- update(m.abun.PoisOffS.23,
+                             seed = SEED,
+                             iter=4000, warmup =1500,
+                             control = list(adapt_delta = 0.999,
+                                            max_treedepth = 15))
+
+pp_check(m.abun.PoisOffS.24, ndraws = 100)
+#underpredicting until mean
+
+save(m.abun.PoisOffS.11, m.abun.PoisOffS.21,
+     m.abun.PoisOffS.12, m.abun.PoisOffS.22,
+     m.abun.PoisOffS.13, m.abun.PoisOffS.23,
+     m.abun.PoisOffS.14, m.abun.PoisOffS.24,
+     file = "./statistics/brms/231106_abundance_OffSet.RData")
+
+
+#### total_nematodes ~ sowndivLogStd*treatment + offset(log(soilDW)) + (1|block/plot), fam=negbinomial ####
+m.abun.nBinomOffS.11 <- brm(total_nematodes ~ sowndivLogStd*treatment + offset(log(soilDW)) + (1|block/plot), 
+                          data = dBEF_nem21, family = "negbinomial",
+                          chains = 3,
+                          cores = 3,
+                          iter = 3000, warmup = 1500,
+                          seed = SEED,
+                          control = list(adapt_delta = 0.9) ) 
+                          #9 divergent transitions
+
+m.abun.nBinomOffS.12 <- update(m.abun.nBinomOffS.11,
+                             seed = SEED,
+                             control = list(adapt_delta = 0.999,
+                                            max_treedepth = 12)) 
+
+m.abun.nBinomOffS.13 <- update(m.abun.nBinomOffS.12,
+                               seed = SEED,
+                               control = list(adapt_delta = 0.999,
+                                              max_treedepth = 15)) 
+
+
+
+summary(m.abun.nBinomOffS.12)
+pp_check(m.abun.nBinomOffS.13, ndraws = 100)
+
+save(m.abun.PoisOffS.11, m.abun.PoisOffS.21,
+     m.abun.PoisOffS.12, m.abun.PoisOffS.22,
+     m.abun.PoisOffS.13, m.abun.PoisOffS.23,
+     m.abun.PoisOffS.14, m.abun.PoisOffS.24,
+     m.abun.nBinomOffS.11,
+     m.abun.nBinomOffS.12,
+     m.abun.nBinomOffS.13,
+     file="./statistics/brms/231106_abundance_OffSetb.RData")
