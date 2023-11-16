545ba7d4f8fc1fa954e67f5471400cee2033b949
cefelix <felix.zeh@gmx.de>
2023-11-16 13:21:13 +0100

    kicked out hurdle_gaussian, as they are wrong
    added "hu~term" (instead of hu~1) hurdle_lognormal models

--- statistics/Predators.R
+++ statistics/Predators.R
@@ -25,60 +25,9 @@ dBEF_nemSH19 <- subset(dBEF_nem, SH == 19)
 
 dBEF_nem21 <- subset(dBEF_nem, year==2021)
 
-#### hurdle: Om_per100gLog ~ sowndivLogStd*treatment + (1|block/plot), family = hurdle_gaussian ####
-m.Pr.hurdle11 <- brm(
-  bf(Pr_per100gLog.hurdle ~ sowndivLogStd*treatment + (1|block/plot),
-     hu ~ 1),
-  data = dBEF_nem21, 
-  family = hurdle_gaussian,
-  stanvars = stanvars, #necessary to use custom brms families!
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99))
-
-pp_check(m.Pr.hurdle11, ndraws=100)
-
-
-#### hurdle: Pr_per100gLog ~ sowndiv*treatment + (1|block/plot), fam=hurdle_gaussian ####
+#### 11a hurdle: Pr_per100g ~ sowndiv*treatment + (1|block/plot), fam=hurdle_lognormal ####
 SEED = 19111996
-m.Pr.hurdle21 <- brm(
-  bf(Pr_per100gLog.hurdle ~ sowndiv*treatment + (1|block/plot),
-     hu ~ 1),
-  data = dBEF_nem21, 
-  family = hurdle_gaussian,
-  stanvars = stanvars, #necessary to use custom brms families!
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99))
-
-m.Pr.hurdle22 <- update(m.Pr.hurdle21,
-                        control = list(adapt_delta = 0.999))
-
-pp_check(m.Pr.hurdle22, ndraws=100)
-
-#### hurdle: Pr_per100g ~ sowndivLogStd*treatment + (1|block/plot), fam=hurdle_lognormal ####
-SEED = 19111996
-m.Pr.hurdle31 <- brm(
-  bf(Pr_per100g ~ sowndivLogStd*treatment + (1|block/plot),
-     hu ~ 1),
-  data = dBEF_nem21, 
-  family = hurdle_lognormal,
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99))
-
-pp_check(m.Pr.hurdle31, ndraws=100)+
-  xlim(0,300)
-
-#### hurdle: Pr_per100g ~ sowndiv*treatment + (1|block/plot), fam=hurdle_lognormal ####
-SEED = 19111996
-m.Pr.hurdle41 <- brm(
+m.Pr.hurdle11a <- brm(
   bf(Pr_per100g ~ sowndiv*treatment + (1|block/plot),
      hu ~ 1),
   data = dBEF_nem21, 
@@ -89,41 +38,35 @@ m.Pr.hurdle41 <- brm(
   seed = SEED,
   control = list(adapt_delta=0.99))
 
-m.Pr.hurdle42 <- update(m.Pr.hurdle41,
-  control=list(adapt_delta=0.999)
+m.Pr.hurdle12a <- update(m.Pr.hurdle11a,
+                        control=list(adapt_delta=0.999)
 )
 
-pp_check(m.Pr.hurdle42, ndraws=100)+
+pp_check(m.Pr.hurdle12a, ndraws=100)+
   xlim(0,300)
 
-#### 51 hurdle: Pr_per100gLog.hurdle ~ sowndivLog*treatment + (1|block/plot), fam=hurdle_gaussian ####
+
+
+#### 11b hurdle: Pr_per100g ~ sowndivLogStd*treatment + (1|block/plot), fam=hurdle_lognormal ####
 SEED = 19111996
-m.Pr.hurdle51b <- brm(
-  bf(Pr_per100gLog.hurdle ~ sowndivLog*treatment + (1|block/plot),
-     hu ~ sowndivLog*treatment + (1|block/plot)), #should predict 
+m.Pr.hurdle11b <- brm(
+  bf(Pr_per100g ~ sowndivLogStd*treatment + (1|block/plot),
+     hu ~ 1),
   data = dBEF_nem21, 
-  family = hurdle_gaussian,
-  stanvars = stanvars, #necessary to use custom brms families!
+  family = hurdle_lognormal,
   chains = 3,
   cores = 3,
   iter = 2000, warmup = 1000,
   seed = SEED,
   control = list(adapt_delta=0.99))
 
-pp_check(m.Pr.hurdle51, ndraws=100)
+pp_check(m.Pr.hurdle11b, ndraws=100)+
+  xlim(0,300)
 
 
-emt = emtrends(m.Pr.hurdle51, "treatment", var="sowndivLog")
-summary(emt, point.est=mean)
-summary(emt, point.est=mean, level = .9) #get slopes
-
-emt.pairs <- pairs(emt)
-summary(emt.pairs, point.est=mean, level = .95) #get differences in slopes betweem treatments
-bayestestR::p_direction(emt.pairs) #probability of difference between the treatments in the output to be negative
-bayestestR::p_direction(m.Pr.hurdle51)
-#### 61 hurdle: Pr_per100gLog.hurdle ~ sowndivLog*treatment + (1|block/plot), fam=hurdle_gaussian ####
+#### 11c hurdle: Pr_per100g ~ sowndivLog*treatment + (1|block/plot), fam=hurdle_lognormal ####
 SEED = 19111996
-m.Pr.hurdle61 <- brm(
+m.Pr.hurdle11c <- brm(
   bf(Pr_per100g ~ sowndivLog*treatment + (1|block/plot),
      hu ~ 1),
   data = dBEF_nem21, 
@@ -134,10 +77,63 @@ m.Pr.hurdle61 <- brm(
   seed = SEED,
   control = list(adapt_delta=0.99))
 
-pp_check(m.Pr.hurdle61, ndraws=100)+
+pp_check(m.Pr.hurdle11c, ndraws=100)+
   xlim(0,300)
 
+####21 hurdle: Pr_per100g ~ sowndivLog*treatment + (1|block/plot), hu~term, fam=hurdle_lognormal ####
+SEED = 19111996
+m.Pr.hurdle21 <- brm(
+  bf(Pr_per100g ~ sowndivLog*treatment + (1|block/plot),
+     hu ~  sowndivLog*treatment + (1|block/plot)),
+  data = dBEF_nem21, 
+  family = hurdle_lognormal,
+  chains = 3,
+  cores = 3,
+  iter = 2000, warmup = 1000,
+  seed = SEED,
+  control = list(adapt_delta=0.99))
 
+pp_check(m.Pr.hurdle71, ndraws=100)+
+  xlim(0,300) 
+
+#model predictions:
+  predictions <- conditional_effects(m.Pr.hurdle71)[[3]]
+  predictions$estimate__
+
+#plot results
+  ggplot(dBEF_nem21, aes(x=sowndivLog, y=Pr_per100g, col=treatment) )+
+    geom_jitter(width=0.3)+
+    geom_smooth(data=predictions, aes(x= sowndivLog, y=estimate__, col=treatment))
+
+#### 31 hurdle: Pr_per100g ~ sowndivLog*treatment + (1|block/plot), hu~term, fam=hurdle_lognormal, no 60 sp. plots ####
+  
+  
+  dat = dBEF_nem21 %>% filter(sowndiv != 60)
+  SEED = 19111996
+  m.Pr.hurdle81 <- brm(
+    bf(Pr_per100g ~ sowndivLog*treatment + (1|block/plot),
+       hu ~  sowndivLog*treatment + (1|block/plot)),
+    data = dat, 
+    family = hurdle_lognormal,
+    chains = 3,
+    cores = 3,
+    iter = 2000, warmup = 1000,
+    seed = SEED,
+    control = list(adapt_delta=0.99))
+  
+  pp_check(m.Pr.hurdle81, ndraws = 100)
+  
+#model predictions:
+  predictions <- conditional_effects(m.Pr.hurdle81)[[3]]
+  predictions$estimate__
+  
+  #plot results
+  ggplot(dat, aes(x=sowndivLog, y=Pr_per100g, col=treatment) )+
+    geom_jitter(width=0.3)+
+    geom_smooth(data=predictions, aes(x= sowndivLog, y=estimate__, col=treatment))
+  
+  summary(m.Pr.hurdle81)
+  
 
 ####comparison - waic ####
 
@@ -147,16 +143,10 @@ loo(m.Pr.hurdle31, m.Pr.hurdle42)
 waic(m.Pr.hurdle31)
 loo(m.Pr.hurdle31)
 #### save hurdle models ####
-save(m.Pr.hurdle11, #Pr.Log ~ sowndivLogStd, fam=hurdle_gaussian
-     m.Pr.hurdle21, m.Pr.hurdle22, #Pr.Log ~ sowndiv, fam=hurdle_gaussian
-     m.Pr.hurdle31, #Pr ~ sowndivLogStd, fam=hurdle_lognormal
-     m.Pr.hurdle42, #Pr ~ sowndiv, fam=hurdle_lognormal
-     m.Pr.hurdle51, #Pr.Log ~ sowndivLog, fam=hurdle_gaussian
-     m.Pr.hurdle61, #Pr ~ sowndivLog, fam=hurdle_lognormal
-     
-     
-     file = "./statistics/brms/231108_Pr_hurdle.RData")
+save(#m.Pr.hurdle11a, m.Pr.hurdle11b, m.Pr.hurdle11c, #Pr.Log ~ sowndivLogStd, fam=hurdle_gaussian
+     m.Pr.hurdle21,
+     m.Pr.hurdle31,
+     file = "./statistics/brms/231116_Pr_hurdle.RData")
 
 load(file = "./statistics/brms/231108_Pr_hurdle.RData")
-conditional_effects(m.Pr.hurdle51)
 
