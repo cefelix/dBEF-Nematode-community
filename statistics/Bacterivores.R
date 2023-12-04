43f9570c6fcfa16dea40b7c88a7b9c029933cf5b
cefelix <felix.zeh@gmx.de>
2023-12-04 15:43:05 +0100

    removing models which: hu~1 OR use non-std predictor OR include 60 sp plots

--- statistics/Bacterivores.R
+++ statistics/Bacterivores.R
@@ -47,145 +47,6 @@ grid.arrange(p.1, p.5, p.15, p.19)
 rm(p.1, p.5, p.15, p.19,
    dBEF_nemSH19, dBEF_nemSH15, dBEF_nemSH5, dBEF_nemSH1)
 
-
-
-#### 11a hurdle: Ba_per100g ~ sowndivLogStd*treatment + (1|block/plot), hu~1, family = hurdle_lognormal ####
-SEED = 22061996
-m.Ba.hurdle11a <- brm(
-  bf(Ba_per100g ~ sowndivLogStd*treatment + (1|block/plot),
-     hu ~ 1),
-  data = dBEF_nem21, 
-  family = hurdle_lognormal,
-  stanvars = stanvars, #necessary to use custom brms families!
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99))
-
-pp_check(m.Ba.hurdle11a, ndraws=100)+
-  xlim(0,1000)
-
-#### 11b hurdle: Ba_per100g ~ sowndivLogStd*treatment + (1|block/plot), hu~term, family = hurdle_lognormal ####
-SEED = 22061996
-m.Ba.hurdle11b <- brm(
-  bf(Ba_per100g ~ sowndivLogStd*treatment + (1|block/plot),
-     hu ~ sowndivLogStd*treatment + (1|block/plot)),
-  data = dBEF_nem21, 
-  family = hurdle_lognormal,
-  stanvars = stanvars, #necessary to use custom brms families!
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99))
-
-
-#### 31a hurdle: Ba_per100g ~ sowndivLogStd*treatment + (1|block/plot), hu~1, family = hurdle_lognormal, no 60 sp. plots ####
-SEED = 22061996
-#exclude 60 sp.:
-  dat <- subset(dBEF_nem21, sowndiv != 60) 
-  #standardize:  
-  dat <- dat %>% mutate(sowndivLogStd = ( (sowndivLog - mean(sowndivLog)) / sd(sowndivLog) ),
-                 .after = sowndivLog)
-
-m.Ba.hurdle31a <- brm(
-  bf(Ba_per100g ~ sowndivLogStd*treatment + (1|block/plot),
-     hu ~ 1),
-  data = dat, 
-  family = hurdle_lognormal,
-  stanvars = stanvars, #necessary to use custom brms families!
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99)) #all good
-
-pp_check(m.Ba.hurdle31a, ndraws=100)+
-  xlim(0,1000)
-
-#### 31a_Ba1-4 hurdle: Ba1-4 ~ sowndivLogStd*treatment + (1|block/plot), hu~1, family = hurdle_lognormal, no 60 sp. plots ####
-SEED = 22061996
-dat <- subset(dBEF_nem21, sowndiv != 60)
-#standardize:  
-dat <- dat %>% mutate(sowndivLogStd = ( (sowndivLog - mean(sowndivLog)) / sd(sowndivLog) ),
-                      .after = sowndivLog)
-
-  (dat$Ba1!=0) %>% sum() # 119 of 228
-  (dat$Ba2!=0) %>% sum() # 206 of 228 
-  (dat$Ba3!=0) %>% sum() #  10 of 228 probably wont work
-  (dat$Ba4!=0) %>% sum() #  89 of 228
-  (dat$Ba5!=0) %>% sum() #   0 of 228  
-
-#the models for Bacterivores in cp1-cp4
-m.Ba1.hurdle31a <- brm(
-  bf(Ba1 ~ sowndivLogStd*treatment + (1|block/plot),
-     hu ~  1),
-  data = dat, 
-  family = hurdle_lognormal,
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99)) 
-  #all good
-
-  pp_check(m.Ba1.hurdle31a, ndraws = 100)+
-    xlim(0,200) #thats okay
-
-m.Ba2.hurdle31a <- brm(
-  bf(Ba2 ~ sowndivLogStd*treatment + (1|block/plot),
-     hu ~  1),
-  data = dat, 
-  family = hurdle_lognormal,
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99)) 
-
-pp_check(m.Ba2.hurdle31a, ndraws = 100)+
-  xlim(0,600) #okay
-
-m.Ba3.hurdle31a <- brm(
-  bf(Ba3 ~ sowndivLogStd*treatment + (1|block/plot),
-     hu ~  1),
-  data = dat, 
-  family = hurdle_lognormal,
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99)) 
-  #62 divergent transitions, 1592 exceedeed max_treedepth, max Rhat=1.37
-
-dat$Ba3 %>% density() %>% plot()
-m.Ba3.hurdle32a <- update(m.Ba3.hurdle31a,
-                          iter = 4000, warmup = 2000,
-                          control = list(adapt_delta=0.999,
-                                         max_treedepth=12))
- #72 divergent transizions
-
-pp_check(m.Ba3.hurdle31a, ndraws = 100)+xlim(0,10) #that's completely off
-
-
-m.Ba4.hurdle31a <- brm(
-  bf(Ba4 ~ sowndivLogStd*treatment + (1|block/plot),
-     hu ~  1),
-  data = dat, 
-  family = hurdle_lognormal,
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99))  #3 divergent transitions
-
-m.Ba4.hurdle32a <- update(m.Ba4.hurdle31a, 
-                          control=list(adapt_delta=0.999))
-
-pp_check(m.Ba4.hurdle32a, ndraws = 100)+
-  xlim(0,50) #bearable
-
 #### 31b hurdle: Ba_per100g ~ sowndivLogStd*treatment + (1|block/plot), hu~term, family = hurdle_lognormal, no 60 sp. plots ####
 SEED = 22061996
 dat <- subset(dBEF_nem21, sowndiv != 60)
@@ -215,6 +76,11 @@ dat <- subset(dBEF_nem21, sowndiv != 60)
 dat <- dat %>% mutate(sowndivLogStd = ( (sowndivLog - mean(sowndivLog)) / sd(sowndivLog) ),
                       .after = sowndivLog)
 
+sum(dat$Ba1 == 0) #109
+sum(dat$Ba2 == 0) #22
+sum(dat$Ba3 == 0) #218 of 228
+sum(dat$Ba4 == 0) #139
+
 m.Ba1.hurdle31b <- brm(
   bf(Ba1 ~ sowndivLogStd*treatment + (1|block/plot),
      hu ~  sowndivLogStd*treatment + (1|block/plot)),
@@ -243,26 +109,7 @@ m.Ba2.hurdle31b <- brm(
 pp_check(m.Ba2.hurdle31b, ndraws=100)+
   xlim(0,700) 
 
-m.Ba3.hurdle31b <- brm(
-  bf(Ba3 ~ sowndivLogStd*treatment + (1|block/plot),
-     hu ~  sowndivLogStd*treatment + (1|block/plot)),
-  data = dat, 
-  family = hurdle_lognormal,
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99)) 
-#86 divergent transitions, 1934 exceeded max_treedepth, bulk ESS too low
-
-  m.Ba3.hurdle32b <- update(m.Ba3.hurdle31b,
-                            iter = 4000, warmup = 2000,
-                            control=list(adapt_delta=0.999, 
-                                         max_treedepth=15)) 
-  
-  pp_check(m.Ba3.hurdle31b, ndraws=100)+
-    xlim(0,40) 
-  
+#skipping Ba-cp3 as there are only 10 samples with a non-zero density
 
 m.Ba4.hurdle31b <- brm(
   bf(Ba4 ~ sowndivLogStd*treatment + (1|block/plot),
@@ -303,83 +150,9 @@ pp_check(m.Ba.hurdle31b, ndraws=100)+
 loo_compare(m.Ba.hurdle31a, m.Ba.hurdle31b) 
 
 
-#### 41a hurdle: Ba_per100g ~ sowndivLog*treatment + (1|block/plot), hu~1, family = hurdle_lognormal, no 60 sp. plots ####
-SEED = 22061996
-dat <- subset(dBEF_nem21, sowndiv != 60)
-#standardize:  
-dat <- dat %>% mutate(sowndivLogStd = ( (sowndivLog - mean(sowndivLog)) / sd(sowndivLog) ),
-                      .after = sowndivLog)
-
-m.Ba.hurdle41a <- brm(
-  bf(Ba_per100g ~ sowndivLog*treatment + (1|block/plot),
-     hu ~ 1),
-  data = dat, 
-  family = hurdle_lognormal,
-  stanvars = stanvars, #necessary to use custom brms families!
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99))
-
-pp_check(m.Ba.hurdle41a, ndraws=100)+
-  xlim(0,1000)
-
-#### 41b hurdle: Ba_per100g ~ sowndivLog*treatment + (1|block/plot), hu~term, family = hurdle_lognormal, no 60 sp. plots ####
-SEED = 22061996
-dat <- subset(dBEF_nem21, sowndiv != 60)
-#standardize:  
-dat <- dat %>% mutate(sowndivLogStd = ( (sowndivLog - mean(sowndivLog)) / sd(sowndivLog) ),
-                      .after = sowndivLog)
-
-m.Ba.hurdle41b <- brm(
-  bf(Ba_per100g ~ sowndivLog*treatment + (1|block/plot),
-     hu ~ sowndivLog*treatment + (1|block/plot)),
-  data = dat, 
-  family = hurdle_lognormal,
-  stanvars = stanvars, #necessary to use custom brms families!
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99))
-
-pp_check(m.Ba.hurdle41b, ndraws = 100)+
-  xlim(0,1000)
-
-####51a hurdle: Ba ~ sowndivLogStd*treatment*week + (1|block/plot), hu~1, family=lognormal####
-SEED = 22061996
-dat <- subset(dBEF_nem21, sowndiv != 60)
-#standardize:  
-dat <- dat %>% mutate(sowndivLogStd = ( (sowndivLog - mean(sowndivLog)) / sd(sowndivLog) ),
-                      .after = sowndivLog)
 
 
-m.Ba.hurdle51a <- brm(
-  bf(Ba_per100g ~ sowndivLogStd + treatment + week + 
-       sowndivLogStd:treatment + sowndivLogStd:week + treatment:week +
-       sowndivLogStd:treatment:week + (1|block/plot),
-     hu ~ 1),
-  data = dat, 
-  family = hurdle_lognormal,
-  chains = 3,
-  cores = 3,
-  iter = 2000, warmup = 1000,
-  seed = SEED,
-  control = list(adapt_delta=0.99)) # 2 divergent transitions
-
-m.Ba.hurdle52a <- update(m.Ba.hurdle51a ,
-                         control=list(adapt_delta=0.999)) #all good
-
-summary(m.Ba.hurdle52a)
-emt <- emtrends(m.Ba.hurdle52a, specs = c("treatment", "week"), var="sowndivLogStd")
-summary(emt, point.est=mean)
-summary(emt, point.est=mean, level = .9) 
-
-emt2 <- emtrends(m.Ba.hurdle52a, specs = "treatment", var="sowndivLogStd")
-summary(emt2, point.est=mean, level = .9)
-
-####51b hurdle: Ba ~ sowndivLogStd*treatment*week + (1|block/plot), hu~term, family=lognormal####
+####51b hurdle: Ba ~ sowndivLogStd*treatment*week + (1|block/plot), hu~term, family=hurdle_lognormal, no 60 sp. plots####
 m.Ba.hurdle51b <- brm(
   bf(Ba_per100g ~ sowndivLogStd + treatment + week + 
        sowndivLogStd:treatment + sowndivLogStd:week + treatment:week +
@@ -400,7 +173,7 @@ pp_check(m.Ba.hurdle51b, ndraws=100)+
 
 summary(m.Ba.hurdle51b)
 
-
+####all 2way interactions####
 m.Ba.hurdle51c <- brm(
   bf(Ba_per100g ~ sowndivLogStd + treatment + week + 
        sowndivLogStd:treatment + sowndivLogStd:week + treatment:week + (1|block/plot),
@@ -421,14 +194,6 @@ m.Ba.hurdle52c <- update(m.Ba.hurdle51c ,
                                       max_treedepth=12)) #4divergent transitions
 pp_check(m.Ba.hurdle52c , ndraws=100)+ 
   xlim(0,1000)
-
-
-#save model
-save(m.Ba.hurdle51a, m.Ba.hurdle52a,
-     m.Ba.hurdle51b,
-     m.Ba.hurdle51c, m.Ba.hurdle52c,
-     file = "./statistics/brms/231129_Ba_week.RData")
-
 
 
 #look at predictions
@@ -472,24 +237,18 @@ save(m.Ba.hurdle51a, m.Ba.hurdle52a,
   rm(dat2)
 
 sum(dat$Ba_per100g ==0)
-#### save hurdle models ####
-save(#m.Ba.hurdle11a,
-     #m.Ba.hurdle11b, 
-     m.Ba.hurdle31a,
-      m.Ba1.hurdle31a,
-      m.Ba2.hurdle31a,
-      m.Ba3.hurdle31a, 
-      m.Ba4.hurdle31a, m.Ba4.hurdle32a,
-     m.Ba.hurdle31b,
+#### savemodels ####
+
+save(  m.Ba.hurdle31b,
        m.Ba1.hurdle31b, 
        m.Ba2.hurdle31b,
-       m.Ba3.hurdle31b, 
+       #Ba-3 skipped, as 10 of 228 samples have a non-zero density
        m.Ba4.hurdle31b, 
      
      #m.Ba.hurdle41a,
      #m.Ba.hurdle41b,
      
-     file="./statistics/brms/231127_Ba.RData")
+     file="./statistics/brms/231204_Ba.RData")
 
 load(file="./statistics/brms/231127_Ba.RData")
 conditional_effects(m.Ba.hurdle11)
