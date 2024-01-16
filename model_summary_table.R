16029bdae2de1b1325bd7058baf250682ed7443e
Felix Zeh <felix.zeh@gmx.de>
2024-01-16 12:55:53 +0100

    created a function to join multiple model summary outputs (mean slope estimate, HPDI, bulk/tail ESS, significancies) into a dataframe

--- model_summary_table.R
+++ model_summary_table.R
@@ -1,55 +1,12 @@
 #creating a neat summary table for all models:
 #packages:
 library(brms)
+library(tidyr)
+library(emmeans)
+library(dplyr)
 
 
-#load models
-load("./statistics/brms/240109_TrophDens_sowndiv_mselect.RData")
-load("./statistics/brms/240109_TrophDens_realdiv_mselect.RData")
-load("./statistics/brms/240110_TrophDens_funcdiv_mselect.RData")
 
-
-#Ba ~ sowndiv
-  Ba.ms <- summary(m.Ba_sowndiv_p5)
-  #extract bulk/tail ESS
-    ESS <- cbind(Ba.ms$spec_pars$Bulk_ESS, 
-               Ba.ms$spec_pars$Tail_ESS)
-    ESS
-    
-  #estimated marginal means for each treatment:
-    Ba.emt.s <- emtrends( m.Ba_sowndiv_p5, specs = c("treatment"), var="sowndivLogStd") %>% 
-      summary(., point.est = mean, level = .95)
-    Ba.emt.s
-    cbind(Ba.emt.s, ESS)
-
-#Ba ~ realdiv
-    Ba.ms <- summary(m.Ba_realdiv_p5)
-    #extract bulk/tail ESS
-    ESS <- cbind(Ba.ms$spec_pars$Bulk_ESS, 
-                 Ba.ms$spec_pars$Tail_ESS)
-    ESS
-    
-    #estimated marginal means for each treatment:
-    Ba.emt.s <- emtrends( m.Ba_realdiv_p5, specs = c("treatment"), var="realdivLogStd") %>% 
-      summary(., point.est = mean, level = .95)
-    Ba.emt.s
-    cbind(Ba.emt.s, ESS)
-    
-#Ba ~ funcdiv
-    Ba.ms <- summary(m.Ba_funcdiv_p5)
-    #extract bulk/tail ESS
-    ESS <- cbind(Ba.ms$spec_pars$Bulk_ESS, 
-                 Ba.ms$spec_pars$Tail_ESS)
-    ESS
-    
-    #estimated marginal means for each treatment:
-    Ba.emt.s <- emtrends( m.Ba_funcdiv_p5, specs = c("treatment"), var="funcdivStd") %>% 
-      summary(., point.est = mean, level = .95)
-    Ba.emt.s
-    cbind(Ba.emt.s, ESS)
-    
-        
-    
 
 #probability of direction:
 Ba.prob_dir <- emtrends(m.Ba_sowndiv_p5, specs = c("treatment"), var="sowndivLogStd") %>%
@@ -57,31 +14,94 @@ Ba.prob_dir <- emtrends(m.Ba_sowndiv_p5, specs = c("treatment"), var="sowndivLog
 Ba.prob_dir
 
 
+#### summarise_models() for TrophDens ~sowndiv, ~realdiv, ~funcdiv #####
+  #load models
+  load("./statistics/brms/240109_TrophDens_sowndiv_mselect.RData")
+  load("./statistics/brms/240109_TrophDens_realdiv_mselect.RData")
+  load("./statistics/brms/240110_TrophDens_funcdiv_mselect.RData")
 
 
 
 
+#a function which creates a table with point estimates and HPDI for regression coefficients for each treatment:
+summarise_models <- function(brmsfit, predictor, level=.95) {
+  response <- deparse(brmsfit$formula)[1] %>% #this row extracts the model term and converts it to a string
+    gsub(".*= (.+) ~.*", "\\1", .) #and this excludes everything which is not the response
 
-
-#### under construction: piping through it #####
-#check out: https://www.youtube.com/watch?v=DZN5nXtEHcg
-
-give_me_a_row <- function(brmsfit, predictor, level=.95) {
-  ms <- summary(brmsfit)
-  ESS <- cbind(ms$spec_pars$Bulk_ESS, 
-               ms$spec_pars$Tail_ESS) 
+  
+  ms <- summary(brmsfit) #extract summary from model
+  ESS <- cbind(ms$spec_pars$Bulk_ESS, #bulk ESS
+               ms$spec_pars$Tail_ESS) #tail ESS
+  colnames(ESS) <- c("bulk ESS", "tail ESS")
+  
   emt.s <- emtrends( brmsfit, specs = c("treatment"), var = predictor) %>% 
-    summary(., point.est = mean, level = level)
-  row <- cbind(emt.s[], ESS, divs)
+    summary(., point.est = mean, level = level) #get slope estimates mean and HPDI
+  emt.s90 <- emtrends( brmsfit, specs = c("treatment"), var = predictor) %>% 
+    summary(., point.est = mean, level = .9)
+  emt.s95 <- emtrends( brmsfit, specs = c("treatment"), var = predictor) %>% 
+    summary(., point.est = mean, level = .9)
+
+  #adding letters, indicating different significance levels: 
+  significance <- rep(NA, nrow(emt.s))
+  for ( i in 1 : nrow(emt.s95)){
+    if( (emt.s95$upper.HPD[i] > 0 & emt.s95$lower.HPD[i] > 0 ) |  #positive relationship (95% CI)
+        (emt.s95$upper.HPD[i] < 0 & emt.s95$lower.HPD[i] < 0 )) { #negative (95% CI)
+      significance[i] = "a"
+    }
+    else if( (emt.s90$upper.HPD[i] > 0 & emt.s90$lower.HPD[i] > 0 ) |  #positive (90% CI)
+             (emt.s90$upper.HPD[i] < 0 & emt.s90$lower.HPD[i] < 0 )) { #negative (90% CI)
+      significance[i] = "b"
+    }
+    
+    
+    else {
+      significance[i] = ""
+    }
+  }
+
+  
+  divs <- 1 #Placeholder: add divergent transitions
+  row <- cbind(response, predictor, emt.s[], ESS, divs, significance)
   return(row)
 }
-  
-give_me_a_row(m.Ba_sowndiv_p5, 
-              predictor = "sowndivLogStd", 
-              level = 0.9)
 
-sown.list <- list(m.Ba_sowndiv_p5, m.Fu_sowndiv_p5, m.Pl_sowndiv_p5, m.Pr_sowndiv_p7, m.Om_sowndiv_p7)
-lapply(sown.list, give_me_a_row, predictor = "sowndivLogStd")
+ 
+#~sowndivLogStd
+  #a list with all final models:
+  sown.list <- list(m.Ba_sowndiv_p5, m.Fu_sowndiv_p5, m.Pl_sowndiv_p5, m.Pr_sowndiv_p7, m.Om_sowndiv_p7)
+  
+  # a df of the model summaries:
+  sowndiv.summary <- lapply(sown.list, summarise_models, predictor = "sowndivLogStd", level =.9) %>% 
+    bind_rows()
+  sowndiv.summary
+  
+  
+#~realdivLogStd    
+  #list of models:
+  real.list <- list(m.Ba_realdiv_p5, m.Fu_realdiv_p5, m.Pl_realdiv_p5, m.Pr_realdiv_p7, m.Om_realdiv_p7)
+
+  # a df of the model summaries:
+  realdiv.summary <- lapply(real.list, summarise_models, predictor = "realdivLogStd", level =.9) %>% 
+    bind_rows()
+  realdiv.summary
+
+#~funcdivLogStd    
+  #list of models:
+  func.list <- list(m.Ba_funcdiv_p5, m.Fu_funcdiv_p5, m.Pl_funcdiv_p5, m.Pr_funcdiv_p7, m.Om_funcdiv_p7)
+  
+  # a df of the model summaries:
+  funcdiv.summary <- lapply(func.list, summarise_models, predictor = "funcdivStd", level =.9) %>% 
+    bind_rows()
+  funcdiv.summary
+  
+  
+#### summarise_models() for Shannon H' ~sowndiv, ~realdiv, ~funcdiv #####
+  #load models
+  load("./statistics/brms/240109_TrophDens_sowndiv_mselect.RData")
+  
+  
+
+
 
 
 #### old ####
